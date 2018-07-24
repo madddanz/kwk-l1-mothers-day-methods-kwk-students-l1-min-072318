d9c3724fef30918a09600cd31914393f97375ce7
madddanz <maddiedanzberger@gmail.com>
2018-07-24 16:03:57 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,4 +1,8 @@
-def holiday_greeting ("holiday=Mother's Day","name=Mom","sender=Your F)
+def holiday_greeting (holiday="Mother's Day",name="Mom",sender="Your Favorite Child")
   "Happy #{holiday}, #{name}!
   From #{sender}!"
 end
+
+holiday_greeting
+
+holiday_greeting("Fourth of July","Beyonce","Jay-Z")
